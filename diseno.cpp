adaf62d1382e8c5c23c5eef9dd40b925f8dcd889
williamFportillo <portillowilliam18@gmail.com>
2017-09-08 11:50:13 -0600

    Create diseno.cpp

---
+++ diseno.cpp
@@ -0,0 +1,31 @@
+#include "diseno.h"
+#include <allegro.h>
+
+void PORTADA(BITMAP* p){
+int parpadeo = 0;
+int y=0;
+int cont = 0;
+bool salida = false;
+bool carga = false;
+
+while(!salida){
+        if(parpadeo < 25) blit(p , screen , 0 , y , 0 , 100 , 600 , 400);
+        else blit(p , screen , 600 , y , 0 , 100 , 600 , 400);
+
+        if(key[KEY_ENTER]){
+            y = 400;
+            carga = true;
+        }
+
+        if(carga) cont++;
+        if(cont > 400) salida = true;
+
+        rest(5);
+        if(++parpadeo == 50) parpadeo = 0;
+}
+    clear_to_color(screen, 0x000000);
+}
+
+void imprimir_fondo(BITMAP* fondo , BITMAP* buffer){
+    masked_blit(fondo , buffer , 0,0,0,0, 600,600);
+}
