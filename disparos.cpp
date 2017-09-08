d9559071a691cafc39fa771d25c87ea82472a04a
williamFportillo <portillowilliam18@gmail.com>
2017-09-08 11:48:25 -0600

    Update disparos.cpp

--- disparos.cpp
+++ disparos.cpp
@@ -1,5 +1,11 @@
 #include "disparos.h"
+#include "personajes.h"
 #include <allegro.h>
+
+bool colicion(int x1 , int y1 , int w1 , int h1 , int x2, int y2, int w2 , int h2){
+
+return( (x1 < x2 + w2 ) && (x2 < x1+w1) && (y1 < y2+h2) && (y2 < y1+h1) );
+}
 
 void crear_bala(int& n_disparos, const int max_disparos ,struct Balas disparos[] ,
                 const int X, const int Y , const int dy)
@@ -29,25 +35,41 @@ void pintar_bala(int& n_disparos, const int max_disparos,struct Balas disparos[]
      }
 
 }
+void eliminar(struct Balas disparos[], int& n_disparos, int cont){
+    Balas Btemp;
+    Btemp = disparos[cont];
+    disparos[cont] = disparos[n_disparos];
+    disparos[n_disparos] = Btemp;
+    n_disparos--;
+    if ( n_disparos < 0 ) n_disparos=0;
+}
 
 void elimina_bala(int& n_disparos, const int max_disparos,struct Balas disparos[],
                   const int ANCHO, const int ALTO)
 {
 
-      Balas Btemp;
+
       if ( n_disparos > 0 && n_disparos < max_disparos){
             for ( int cont = 1; cont <= n_disparos; cont++){
                       if ( disparos[cont].y > ALTO || disparos[cont].y < 0 ||
                            disparos[cont].x > ANCHO|| disparos[cont].x < 0  )
                        {
-                                Btemp = disparos[cont];
-                                disparos[cont] = disparos[n_disparos];
-                                disparos[n_disparos] = Btemp;
-                                n_disparos--;
-                                if ( n_disparos < 0 ) n_disparos=0;
+                             eliminar(disparos, n_disparos, cont);
                        }
             }
       }
 
 
+}
+void eliminar_bala_objeto(struct NAVE& N , struct NAVE& E, struct Balas B[]){
+    if ( N.n_disp > 0 && N.n_disp < N.max_disp){
+            for ( int cont = 1; cont <= N.n_disp; cont++){
+                    if(colicion(E.x , E.y , E.ancho_p, E.alto_p, B[cont].x , B[cont].y , N.ancho_b , N.alto_b) && E.vida>0){
+                        eliminar(B,N.n_disp, cont);
+                        E.vida--;
+
+                    }
+            }
+
+    }
 }
