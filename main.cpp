c7fc522c215d43416ba50b9f6b5b1ecbe9265423
williamFportillo <portillowilliam18@gmail.com>
2017-09-08 11:45:45 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,10 @@
 #include "inicia.h"
 #include "disparos.h"
 #include "personajes.h"
+#include "diseno.h"
 
 #define ANCHO 600
-#define ALTO  375
+#define ALTO  600
 
 
 int main(){
@@ -15,9 +16,13 @@ int main(){
 	inicia_audio(70,70);        //
     //Pantalla principal
     BITMAP *buffer = create_bitmap(ANCHO, ALTO);
+    BITMAP *portada = load_bitmap("Recursos/portada.bmp", NULL);
+    BITMAP *fondo = load_bitmap("Recursos/fondo.bmp", NULL);
+
+    PORTADA(portada);
     //INICIALIZANDO LAS FUNCIONES DE LAS NAVES;
     NAVE nave;
-    nave.inicio("Recursos/nave.bmp","Recursos/Bala2.bmp",nave.ancho_b,nave.alto_b ,30,20 , ANCHO/2 , ALTO-50, -10 , 0);
+    nave.inicio("Recursos/nave.bmp","Recursos/Bala2.bmp",nave.ancho_b,nave.alto_b ,30,20 , ANCHO/2 , ALTO-70, -10 , 0 , 3);
 
     NAVE enemigo[60];
     acomodar_enemigos(enemigo);
@@ -35,11 +40,23 @@ int main(){
                 crear_bala(nave.n_disp , nave.max_disp , disparos , nave.x , nave.y , nave.dir_bala);
             nave.disparar(disparos ,buffer);
 
+            for(int i=0; i < 55;i++)
+               eliminar_bala_objeto(nave,enemigo[i],disparos);
+
+
+            /* codigo con 11
+            for(int i=0; i < 55;i++)
+               if(eliminar_bala_objeto(nave,enemigo[i],disparos))
+                explosion1(enemigo[i],buffer);
+            */
+
             pintar_enemigo(enemigo, buffer, mov);
-            if(enemigo[azar].n_disp == 0) azar = rand()%55;
-            enemigo[azar].disparar(disparos_e , buffer);
+            //if(enemigo[azar].n_disp == 0) azar = rand()%55;
+            //enemigo[azar].disparar(disparos_e , buffer);
             if(enemigo[0].temporizador(10))
             if(++mov == 2) mov =0;
+
+            imprimir_fondo(fondo , buffer);
 
             blit(buffer,screen, 0,0 , 0,0 ,ANCHO,ALTO );
             rest(20);
