0bed244afc98e7cc7663c6e2eccb2a2d9023c808
williamFportillo <portillowilliam18@gmail.com>
2017-09-08 11:49:19 -0600

    Create diseno.h

---
+++ diseno.h
@@ -0,0 +1,7 @@
+#include <allegro.h>
+#ifndef DISENO_H_INCLUDED
+#define DISENO_H_INCLUDED
+
+void PORTADA(BITMAP* p);
+void imprimir_fondo(BITMAP* fondo , BITMAP* buffer);
+#endif // DISENO_H_INCLUDED
